ac273cf30ea298b9f4ab8073cc80b956d09775c1
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-26 20:26:08 -0800

    expose to objc

--- SimplePDF/SimplePDF.swift
+++ SimplePDF/SimplePDF.swift
@@ -40,6 +40,7 @@ public enum ContentAlignment {
         
         pageBounds = CGRect(origin: CGPoint.zero, size: pageSize)
         self.pageMargin = pageMargin
+        super.init()
     }
     
     
